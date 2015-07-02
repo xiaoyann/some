8ab638897272c5af9ba3aa3575eedd3ca0fec144
striver-x <striverx@163.com>
2015-07-02 12:29:21 +0800

    update

--- ZYTab.js
+++ ZYTab.js
@@ -1,46 +1,97 @@
-(function(global) {
-    var tabSelector = '.zytab-nav',
-        contentSelector = '.zytab-content',
-        dataTabIndex = 'tab-index',
-        activeTabClass = 'zytab-nav-active',
+(function(global, factory) {
+    if (typeof module !== 'undefined' && module.exports !== undefined) {
+        var $ = require('jquery');
+        module.exports = factory($);
+
+    } else if (typeof define === 'function' && define.amd) {
+        define(['jquery'], function($) {
+            return factory($);
+        });
+        
+    } else {
+        global.ZYTab = factory(global.jQuery || global.Zepto || global.$);    
+    }
+})(this, function($) {
+    var tabDataName = 'zt',
         defOptions = {
+            lastTab: '',
+            defTab: '',
             event: 'click',
-            lastIndex: 0,
-            defIndex: 0
+            activeClass: 'zt-active'
         };
 
     function ZYTab(options) {
-        var tabBox = $(options.tabBox);
-        this.tabs = $(tabSelector, tabBox);
-        this.contents = $(contentSelector, tabBox);
+        this.tabs = {};
+        this.bodies = {};
+
         $.extend(this, defOptions, options);
-        tabBox.on(this.event, tabSelector, {context: this}, this.handler);
-        this.onChange(this.defIndex);
+
+        $(options.selector).each($.proxy(
+            function(k, el) {
+                var $el = $(el), 
+                    s = $el.data(tabDataName),
+                    k = createKey(s);
+
+                if (this.defTab === '') {
+                    this.defTab = k;
+                }
+
+                if (this.lastTab === '') {
+                    this.lastTab = k;
+                }
+
+                this.tabs[k] = $el;
+                this.bodies[k] = $(s);
+            }
+
+        , this)).parent().on(
+            this.event, 
+            this.selector, 
+            {context: this}, 
+            this.handler
+        );
+
+        this.whenChange(this.defTab);
     }
 
-    $.extend(ZYTab.prototype, {
+    ZYTab.prototype = {
+        constructor: ZYTab,
+
         handler: function(ev) {
-            ev.data.context.onChange($(this).data(dataTabIndex));
+            ev.data.context.whenChange($(this).data(tabDataName));
         },
 
-        onChange: function(index) {
-            if (index !== this.lastIndex) {
-                this.setTab(index);
-                this.setContent(index);
-                this.lastIndex = index;
+        whenChange: function(curTab) {
+            var lastTab = createKey(this.lastTab);
+            curTab = createKey(curTab);
+            if (curTab !== this.lastTab) {
+                this.setTab(curTab, lastTab);
+                this.setBody(curTab, lastTab);
+                this.lastTab = curTab;
             }
         },
 
-        setTab: function(index) {
-            this.tabs.eq(this.lastIndex).removeClass(activeTabClass);
-            this.tabs.eq(index).addClass(activeTabClass);
+        setTab: function(curTab, lastTab) {
+            this.tabs[lastTab].removeClass(this.activeClass);
+            this.tabs[curTab].addClass(this.activeClass);
         },
 
-        setContent: function(index) {
-            this.contents.eq(this.lastIndex).hide();
-            this.contents.eq(index).show();
+        setBody: function(curTab, lastTab) {
+            this.bodies[lastTab].hide();
+            this.bodies[curTab].show();
         }
-    });
+    };
 
-    global.ZYTab = ZYTab;
-})(this);
+    function createKey(s) {
+        return s.replace(/[\s-#\.\[\]]+/g, '');
+    }
+
+    return ZYTab;
+});
+
+
+
+
+
+
+
